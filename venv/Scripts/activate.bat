3698d7800f09653baf4d599b26e936a9d3764a21
Larissa Ferraz <laribferraz@gmail.com>
2024-04-21 21:11:45 -0300

    sistema ajustado

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\pc\OneDrive\Documentos\Projetos Python\apptelemedicina\venv
+set VIRTUAL_ENV=C:\Users\laris\OneDrive\Documentos\MeusProjetos\healing\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
